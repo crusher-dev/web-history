bfb9af71aaf9a8bb21a02eb39e963d24c877613c
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-12-04 09:13:20 +0530

    (BE) Add db class

---
+++ packages/backend/src/config.ts
@@ -0,0 +1,10 @@
+export const BACKBLAZE_CONFIG = {
+    applicationKeyId: "726952fbc7f7",
+    applicationKey: "004fb7d2f9ec4436989538ae1cac921b1af34fa76a",
+    bucket: "web-history",
+};
+
+export const SUPABASE_CONFIG = {
+    url: "https://qzqjxqjxqjxqjxqjxqjx.supabase.co",
+    key: "",
+};
